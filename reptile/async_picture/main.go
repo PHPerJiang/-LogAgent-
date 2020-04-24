a06086a8ecfb528e650e3de548856f4338aa0a19
PHPerJiang <554246432@qq.com>
2020-04-24 20:12:12 +0800

    分页爬取

--- reptile/async_picture/main.go
+++ reptile/async_picture/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
 	"sync"
@@ -15,10 +16,19 @@ var (
 	urlChan chan string
 	wg      sync.WaitGroup
 	regs    = map[string]string{
-		"imgUrl": `<img[\s\S]+?src=['"](http[^'"]+?\.(?:jpg|jpeg|png))[^>]*>`,
+		"imgUrl":  `<img[\s\S]+?src=['"](http[^'"]+?\.(?:jpg|jpeg|png))[^>]*>`,
+		"pageUrl": `<a[\s\S]+?href="(https://wiki.smzdm.com/kongtiao/p[\s\S]+?\/)"`,
 	}
-	taskNum int = 10
+	pageMap  map[string]string
+	imageMap map[string]string
+	taskNum  int = 10
 )
+
+func init() {
+	urlChan = make(chan string, 10000)
+	pageMap = make(map[string]string, 0)
+	imageMap = make(map[string]string, 0)
+}
 
 //处理错误信息
 func handleEerr(err error) {
@@ -35,11 +45,21 @@ func getImgUrls(url string) {
 	handleEerr(err)
 	body, err := ioutil.ReadAll(resp.Body)
 	handleEerr(err)
+	//匹配页面中的图片
 	re := regexp.MustCompile(regs["imgUrl"])
 	urlSlice := re.FindAllStringSubmatch(string(body), -1)
 	for _, v := range urlSlice {
 		urlChan <- v[1]
 	}
+	//匹配页面中的pageurl
+	re = regexp.MustCompile(regs["pageUrl"])
+	pageUrlSlice := re.FindAllStringSubmatch(string(body), -1)
+	for _, v := range pageUrlSlice {
+		if _, ok := pageMap[v[1]]; !ok {
+			pageMap[v[1]] = v[1]
+		}
+	}
+	os.Exit(0)
 }
 
 //下载图片
@@ -65,8 +85,7 @@ func downImg(taskID int) {
 }
 
 func main() {
-	urlChan = make(chan string, 10000)
-	getImgUrls("https://www.smzdm.com")
+	getImgUrls("https://wiki.smzdm.com/kongtiao/")
 	fmt.Println("共抓取到" + strconv.Itoa(len(urlChan)) + "张图片")
 	for i := 1; i <= taskNum; i++ {
 		wg.Add(1)
