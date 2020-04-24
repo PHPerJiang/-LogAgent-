13b75217a80dc2c40d0a30eac350cec911b71f80
PHPerJiang <554246432@qq.com>
2020-04-24 19:48:13 +0800

    多任务并发下载

--- reptile/async_picture/main.go
+++ reptile/async_picture/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,11 +13,11 @@ import (
 
 var (
 	urlChan chan string
-	wrLock  sync.Mutex
 	wg      sync.WaitGroup
 	regs    = map[string]string{
 		"imgUrl": `<img[\s\S]+?src=['"](http[^'"]+?\.(?:jpg|jpeg|png))[^>]*>`,
 	}
+	taskNum int = 10
 )
 
 //处理错误信息
@@ -42,29 +43,34 @@ func getImgUrls(url string) {
 }
 
 //下载图片
-func downImg(imgUrl string) {
-	wg.Add(1)
-	resp, err := http.Get(imgUrl)
-	defer resp.Body.Close()
-	handleEerr(err)
-	body, err := ioutil.ReadAll(resp.Body)
-	handleEerr(err)
-	filename := "/Users/smzdm/go/src/LogAgent/reptile/async_picture/images/" + strconv.Itoa(int(time.Now().Nanosecond())) + ".jpg"
-	err = ioutil.WriteFile(filename, body, 0644)
-	handleEerr(err)
-	log.Println(filename + " download success!")
+func downImg(taskID int) {
+	fmt.Printf("task %d start...\n", taskID)
+	for {
+		if url, ok := <-urlChan; !ok {
+			break
+		} else {
+			resp, err := http.Get(url)
+			defer resp.Body.Close()
+			handleEerr(err)
+			body, err := ioutil.ReadAll(resp.Body)
+			handleEerr(err)
+			filename := "/Users/smzdm/go/src/LogAgent/reptile/async_picture/images/" + strconv.Itoa(int(time.Now().Nanosecond())) + ".jpg"
+			err = ioutil.WriteFile(filename, body, 0644)
+			handleEerr(err)
+			log.Println(filename + " download success!")
+		}
+	}
+	fmt.Printf("task %d end...\n", taskID)
 	wg.Done()
 }
 
 func main() {
-	urlChan = make(chan string, 10)
-	go getImgUrls("https://www.nvshens.net/g/32446/8.html")
-	for {
-		select {
-		case url := <-urlChan:
-			go downImg(url)
-		default:
-			time.Sleep(time.Second)
-		}
+	urlChan = make(chan string, 10000)
+	getImgUrls("https://www.smzdm.com")
+	fmt.Println("共抓取到" + strconv.Itoa(len(urlChan)) + "张图片")
+	for i := 1; i <= taskNum; i++ {
+		wg.Add(1)
+		go downImg(i)
 	}
+	wg.Wait()
 }
